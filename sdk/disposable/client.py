3367f048496145bea0b2b835ae8efcf39b6f9602
DevBunny-exe <ronnycheng890@gmail.com>
2026-03-05 22:35:04 +0800

    Update client.py

--- sdk/disposable/client.py
+++ sdk/disposable/client.py
@@ -1,7 +1,12 @@
 import requests
 
 API_URL = "http://130.51.23.85/run"
+TOKEN = "exec_secret_123"
 
 def run(script):
-    r = requests.post(API_URL, json={"script": script})
+    r = requests.post(
+        API_URL,
+        json={"script": script},
+        headers={"x-token": TOKEN}
+    )
     return r.json()
