8473902182cf0c1219e8f08b8017453f9c4243d4
DevBunny-exe <ronnycheng890@gmail.com>
2026-03-05 19:49:31 +0800

    Update client.py

--- sdk/disposable/client.py
+++ sdk/disposable/client.py
@@ -5,8 +5,11 @@ API = "http://130.51.23.85:8000/run"
 
 def run(cmd):
     r = requests.post(API, json={"script": cmd})
-    return r.text
+    print(r.text)
 
 def main():
     cmd = " ".join(sys.argv[1:])
     run(cmd)
+
+if __name__ == "__main__":
+    main()
