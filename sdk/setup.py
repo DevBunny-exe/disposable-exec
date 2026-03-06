14055e913669d61b23828781d4a41afa7798073d
DevBunny-exe <ronnycheng890@gmail.com>
2026-03-06 19:58:06 +0800

    Update setup.py

--- sdk/setup.py
+++ sdk/setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="disposable",
-    version="0.1",
+    name="disposable-exec",
+    version="0.1.0",
     packages=find_packages(),
-    install_requires=["requests"],
+    install_requires=[
+        "requests"
+    ],
     entry_points={
         "console_scripts": [
-            "disposable=disposable.client:main"
+            "disposable=disposable.cli:main"
         ]
-    }
+    },
 )
