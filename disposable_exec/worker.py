b9ddd9d988f45b45fca9cabdc05a108f9c2e777b
DevBunny-exe <ronnycheng890@gmail.com>
2026-03-09 12:31:10 +0800

    Update worker.py

--- disposable_exec/worker.py
+++ disposable_exec/worker.py
@@ -6,27 +6,22 @@ import time
 
 from disposable_exec.logs import write_log
 from disposable_exec.queue import dequeue_job
-from disposable_exec.usage import increment_usage
 from disposable_exec.results import save_result
 from disposable_exec.status import set_status
 
 
 def set_limits():
-    # CPU limit (seconds)
     resource.setrlimit(resource.RLIMIT_CPU, (2, 2))
 
-    # Memory limit (128MB)
     memory_limit = 128 * 1024 * 1024
     resource.setrlimit(resource.RLIMIT_AS, (memory_limit, memory_limit))
 
-    # Process limit
     resource.setrlimit(resource.RLIMIT_NPROC, (10, 10))
 
 
 print("Worker started...")
 
 while True:
-
     job = dequeue_job()
 
     if not job:
@@ -35,14 +30,13 @@ while True:
 
     execution_id = job["execution_id"]
     code = job["script"]
-    api_key = job["api_key"]
+    key_id = job["key_id"]
 
     set_status(execution_id, "running")
 
     path = None
 
     try:
-
         with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as f:
             f.write(code.encode())
             path = f.name
@@ -60,7 +54,7 @@ while True:
         duration = time.time() - start
 
         write_log(
-            api_key,
+            key_id,
             result.stdout,
             result.stderr,
             result.returncode,
@@ -78,11 +72,9 @@ while True:
         )
 
         set_status(execution_id, "finished")
-
         print("Job finished")
 
     except Exception as e:
-
         set_status(execution_id, "failed")
 
         save_result(
@@ -96,8 +88,5 @@ while True:
         )
 
     finally:
-
-        increment_usage(api_key)
-
         if path and os.path.exists(path):
             os.remove(path)
